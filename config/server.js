613abcec10516516014af0fe6070ca106c79021d
Timothy Wang <timothy.wang@clsa.com>
2021-04-09 16:45:45 +0800

    Init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1ee7e549c5b99830b1bb057733686e41'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '78614f0e-86b0-4c12-a137-c75302609f23'
+};
